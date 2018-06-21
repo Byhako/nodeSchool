ac2313aa0eb2415a8d22f82a295985377b486587
Ruben E. Acosta <eldragondera1@gmail.com>
2018-06-21 13:23:45 -0500

    learnyounode mi primer IO

---
+++ learnyounode/mi-primer-IO.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+const path = process.argv
+
+// const file = fs.readFileSync(path[2]).toString()
+const file = fs.readFileSync(path[2], 'utf8')
+
+const lineas = file.split('\n')
+
+console.log(lineas.length - 1)
