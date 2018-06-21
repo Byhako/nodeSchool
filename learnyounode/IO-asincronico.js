df3fdf4f5172fbfb3b8715ddbf9dfdc00946abea
Ruben E. Acosta <eldragondera1@gmail.com>
2018-06-21 16:54:06 -0500

    filadro por extension

---
+++ learnyounode/IO-asincronico.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const path = process.argv[2]
+
+fs.readFile(path, 'utf8', (error, data) => {
+  if (error) throw error
+  const lineas = data.split('\n')
+  console.log(lineas.length - 1)
+
+})
+
---
+++ learnyounode/filtrado.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+
+const ruta = process.argv[2]
+const ext = '.' + process.argv[3]
+
+fs.readdir(ruta, (error, data) => {
+  if (error) throw error
+
+  for (let i=0 ; i<data.length ; i++) {
+    if (ext == path.extname(data[i])) {
+      console.log(data[i])
+    }
+  }
+})
